5728db2ab062e32c58a753788194badd4ffbd08a
lunz1207 <lunz1207@gmail.com>
2022-06-13 11:40:28 +0800

    jmx curd

--- src/components/UploadJmx.vue
+++ src/components/UploadJmx.vue
@@ -5,9 +5,9 @@
       <el-aside width="200px">Aside</el-aside>
       <el-container>
         <el-main>
-          <el-upload class="upload-demo" name="fp" action="http://127.0.0.1:8004/upload/" :on-preview="handlePreview"
-            :on-remove="handleRemove" :before-remove="beforeRemove" multiple :limit="3" :on-exceed="handleExceed"
-            :file-list="fileList">
+          <el-upload class="upload-demo" name="file" action="http://127.0.0.1:8004/files/" :on-preview="handlePreview"
+            :on-change="handlechange" :on-remove="handleRemove" :before-remove="beforeRemove" multiple :limit="3"
+            :on-exceed="handleExceed" :file-list="fileList">
             <el-button type="primary">Click to upload</el-button>
             <template #tip>
               <div class="el-upload__tip">
@@ -18,14 +18,10 @@
           <el-table :data="tableData" style="width: 100%">
             <el-table-column fixed prop="date" label="Date" width="150" />
             <el-table-column prop="name" label="Name" width="120" />
-            <el-table-column prop="state" label="State" width="120" />
-            <el-table-column prop="city" label="City" width="120" />
-            <el-table-column prop="address" label="Address" width="600" />
-            <el-table-column prop="zip" label="Zip" width="120" />
+            <el-table-column prop="type" label="type" width="120" />
             <el-table-column fixed="right" label="Operations" width="120">
-              <template #default>
-                <el-button link type="primary" size="small" @click="one">run</el-button>
-                <el-button link type="primary" size="small" @click="two">delete</el-button>
+              <template #default="scope">
+                <el-button link type="primary" size="small" @click="deleteFile(scope.$index)">delete</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -39,6 +35,23 @@
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
 
+const tableData = ref([])
+
+const getList = async () => {
+  const resp = await axios.get('http://127.0.0.1:8004/files')
+
+  const td = []
+  for (var i = 0; i < resp.data.details.length; i++) {
+    const c = {
+      date: '2016-05-03',
+      name: resp.data.details[i],
+      type: 'jmx'
+    }
+    td.push(c)
+  }
+  tableData.value = td
+}
+
 export default {
   name: 'UploadJmx',
   data() {
@@ -47,51 +60,29 @@ export default {
     }
   },
   props: {
-    msg: String,
+    msg: String
   },
   setup() {
-    const tableData = ref([])
     onMounted(() => {
       getList()
 
     })
-    const getList = async () => {
-      const resp = await axios.get('http://127.0.0.1:8004/file_list')
-      tableData.value = [{
-        date: '2016-05-03',
-        name: resp.data[0],
-        state: 'Completed',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-        tag: 'Home',
-      },
-      {
-        date: '2016-05-03',
-        name: resp.data[1],
-        state: 'Running',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-        tag: 'Home',
-      }]
-    }
-
     return {
       tableData
     }
   },
 
   methods: {
-    async one() {
-      await axios.post('http://127.0.0.1:8003/jobs/jmx')
-      alert('创建成功')
-
+    async handlechange() {
+      getList()
     },
-    async two() {
-      await axios.delete('http://127.0.0.1:8003/jobs/jmx')
-      alert('删除成功')
-
+    async handleRemove() {
+    },
+    async deleteFile(index) {
+      alert('确认删除？')
+      const name = tableData.value[index].name
+      await axios.delete(`http://127.0.0.1:8004/files/${name}`)
+      getList()
     },
   }
 }
