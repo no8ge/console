5c726c3d050848c8842c880214c3c12113c4e20a
lunz1207 <lunz1207@gmail.com>
2022-06-16 17:09:07 +0800

    feat: analysis

--- src/components/UploadJmx.vue
+++ src/components/UploadJmx.vue
@@ -2,13 +2,17 @@
   <el-container>
     <el-header>{{ jmx }} {{ msg }}</el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <!-- <el-aside width="200px">Aside</el-aside> -->
       <el-container>
+        <el-main>
+          jmx
+        </el-main>
+
         <el-main>
           <el-upload class="upload-demo" name="file" action="http://tink.com:31695/files" :on-preview="handlePreview"
             :on-change="handlechange" :on-remove="handleRemove" :before-remove="beforeRemove" multiple :limit="3"
             :on-exceed="handleExceed" :file-list="fileList">
-            <el-button type="primary">Click to upload</el-button>
+            <el-button type="primary">Click to upload jmx</el-button>
             <template #tip>
               <div class="el-upload__tip">
                 jpg/png files with a size less than 500KB.
@@ -22,11 +26,48 @@
             <el-table-column fixed="right" label="Operations" width="120">
               <template #default="scope">
                 <el-button link type="primary" size="small" @click="deleteFile(scope.$index)">delete</el-button>
+                <el-button link type="primary" size="small" @click="runJmx(scope.$index)">run</el-button>
               </template>
             </el-table-column>
           </el-table>
         </el-main>
-        <el-footer>Footer</el-footer>
+
+        <el-main>
+          job
+        </el-main>
+
+        <el-main>
+          <el-table :data="jobs" style="width: 100%" align="center">
+            <el-table-column fixed prop="date" label="Date" width="150" />
+            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="type" label="type" width="120" />
+            <el-table-column fixed="right" label="Operations" width="120">
+              <template #default="scope">
+                <el-button link type="primary" size="small" @click="deleteJmx(scope.$index)">delete</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-main>
+
+
+        <el-footer>metrics</el-footer>
+
+        <el-button type="success" align="center" @click="getReport">getMetrics</el-button>
+
+        <el-table :data="metrics" style="width: 100%" align="center">
+          <el-table-column fixed prop="application" label="application" width="120" />
+          <el-table-column prop="result" label="result" width="120" />
+          <el-table-column prop="statut" label="statut" width="120" />
+          <el-table-column prop="table" label="table" width="120" />
+          <el-table-column prop="transaction" label="transaction" width="120" />
+          <el-table-column prop="_field" label="_field" width="120" />
+          <el-table-column prop="_measurement" label="_measurement" width="120" />
+          <el-table-column prop="_start" label="_start" width="120" />
+          <el-table-column prop="_stop" label="_stop" width="120" />
+          <el-table-column prop="_time" label="_time" width="120" />
+          <el-table-column prop="_value" label="_value" width="120" />
+        </el-table>
+
       </el-container>
     </el-container>
   </el-container>
@@ -36,6 +77,8 @@ import axios from 'axios';
 import { ref, onMounted } from 'vue';
 
 const tableData = ref([])
+const jobs = ref([])
+const metrics = ref([])
 
 const getList = async () => {
   const resp = await axios.get('http://tink.com:31695/files')
@@ -52,6 +95,22 @@ const getList = async () => {
   tableData.value = td
 }
 
+
+const getJobs = async () => {
+  const resp = await axios.get('http://tink.com:31695/tink/jobs')
+
+  const td = []
+  for (var i = 0; i < resp.data.length; i++) {
+    const c = {
+      date: '2016-05-03',
+      name: resp.data[i],
+      type: 'job'
+    }
+    td.push(c)
+  }
+  jobs.value = td
+}
+
 export default {
   name: 'UploadJmx',
   data() {
@@ -65,10 +124,13 @@ export default {
   setup() {
     onMounted(() => {
       getList()
+      getJobs()
 
     })
     return {
-      tableData
+      tableData,
+      jobs,
+      metrics
     }
   },
 
@@ -83,6 +145,22 @@ export default {
       const name = tableData.value[index].name
       await axios.delete(`http://tink.com:31695/files/${name}`)
       getList()
+    },
+    async runJmx(index) {
+      const name = tableData.value[index].name
+      await axios.post('http://tink.com:31695/tink/job', { name: name, jmx: `/jmx/${name}` })
+      getJobs()
+      alert('运行成功')
+    },
+    async deleteJmx(index) {
+      alert('确认删除？')
+      const name = jobs.value[index].name
+      await axios.delete(`http://tink.com:31695/tink/job/${name}`)
+      getJobs()
+    },
+    async getReport() {
+      const resp = await axios.post('http://tink.com:31695/analysis')
+      metrics.value.push(resp.data)
     },
   }
 }
