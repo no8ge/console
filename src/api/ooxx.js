530736c2493701c736706ffa9d1087acf4892c67
lunz1207 <lunz1207@gmail.com>
2022-11-13 22:17:07 +0800

    feat: baseurl

--- src/api/ooxx.js
+++ src/api/ooxx.js
@@ -4,8 +4,10 @@ var url;
 
 if (process.env.NODE_ENV == "development") {
   url = "/api";
+} else {
+  url = "http://" + process.env.VUE_APP_host + process.env.VUE_APP_port;
 }
-
+console.log(url);
 export const requestInstance = axios.create({
   baseURL: url,
 });
